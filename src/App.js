5e6a71d099fa296d02feffcc2ad6624c3512d318
LIU Heyang <lhypds@gmail.com>
2024-01-27 22:52:48 +0900

    Reproduce

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t } = useTranslation();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* This is no problem */}
+          <p>{t('Clear output and reset session. (`:reset`)')}</p>
+          
+          {/* This is cut off from the middle */}
+          <p>{t('Fullscreen. (`:fullscreen [off?]`)')}</p>
         </p>
         <a
           className="App-link"
---
+++ src/i18n/i18n.js
@@ -0,0 +1,26 @@
+// src/i18n/i18n.js
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+    detection: {
+      order: ['queryString', 'cookie', 'localStorage', 'path', 'subdomain'],
+      caches: ['cookie'],
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
